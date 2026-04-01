9661f8d52ce1c72768f3c062fda4d3648e78bd87
Ninii <eaninika@gmail.com>
2026-04-01 12:13:48 -0300

    awffw

---
+++ cadastroAluno/cadastroAluno.php
@@ -0,0 +1,31 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro Aluno</title>
+</head>
+<body>
+<h1>Cadastro Aluno</h1>
+
+<form action="outrapagina.php">
+    <label for="name">Nome:</label><br>
+    <input type="text" id="name" name="name"><br>
+
+    <label for="email">Email</label><br>
+    <input type="text" id="email" name="email"><br>
+
+    <label for="cell">Celular</label><br>
+    <input type="text" id="cell" name="cell"><br>
+
+    <label for="regAluno">Registro Aluno:</label><br>
+    <input type="text" id="regAluno" name="regAluno"><br>
+
+
+    <input type="submit" value="Submit">
+</form>
+</body>
+</html>
+
